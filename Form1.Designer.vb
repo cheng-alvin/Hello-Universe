21be6d0bb1de09796d91bc7748fd0cfffafa4272
Alvin Cheng <eventide1029@gmail.com>
2025-02-27 10:57:40 +1100

    Add project files.

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -28,11 +28,24 @@ Partial Class Form1
         clear1 = New Button()
         Label1 = New Label()
         Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        Button5 = New Button()
+        Button9 = New Button()
+        Button10 = New Button()
+        Button14 = New Button()
+        Button6 = New Button()
+        Button7 = New Button()
+        Button12 = New Button()
+        Button13 = New Button()
+        Button8 = New Button()
+        Button11 = New Button()
         SuspendLayout()
         ' 
         ' helloBtn
         ' 
-        helloBtn.Location = New Point(29, 56)
+        helloBtn.Location = New Point(29, 25)
         helloBtn.Name = "helloBtn"
         helloBtn.Size = New Size(162, 45)
         helloBtn.TabIndex = 0
@@ -48,7 +61,7 @@ Partial Class Form1
         ' 
         ' exitBtn
         ' 
-        exitBtn.Location = New Point(29, 315)
+        exitBtn.Location = New Point(29, 544)
         exitBtn.Name = "exitBtn"
         exitBtn.Size = New Size(162, 45)
         exitBtn.TabIndex = 2
@@ -57,11 +70,11 @@ Partial Class Form1
         ' 
         ' clear1
         ' 
-        clear1.Location = New Point(29, 184)
+        clear1.Location = New Point(29, 98)
         clear1.Name = "clear1"
         clear1.Size = New Size(162, 45)
         clear1.TabIndex = 3
-        clear1.Text = "Clear"
+        clear1.Text = "Clear 1"
         clear1.UseVisualStyleBackColor = True
         ' 
         ' Label1
@@ -75,18 +88,148 @@ Partial Class Form1
         ' 
         ' Button1
         ' 
-        Button1.Location = New Point(29, 120)
+        Button1.Location = New Point(29, 149)
         Button1.Name = "Button1"
         Button1.Size = New Size(162, 45)
         Button1.TabIndex = 6
-        Button1.Text = "Goodbye"
+        Button1.Text = "Clear 2"
         Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(29, 234)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(162, 45)
+        Button2.TabIndex = 7
+        Button2.Text = "ReadOnly"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(29, 285)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(162, 45)
+        Button3.TabIndex = 8
+        Button3.Text = "Multiline"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Location = New Point(29, 355)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(162, 45)
+        Button4.TabIndex = 9
+        Button4.Text = "Text Align"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' Button5
+        ' 
+        Button5.Location = New Point(29, 406)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(162, 45)
+        Button5.TabIndex = 10
+        Button5.Text = "BorderStyle"
+        Button5.UseVisualStyleBackColor = True
+        ' 
+        ' Button9
+        ' 
+        Button9.Location = New Point(559, 369)
+        Button9.Name = "Button9"
+        Button9.Size = New Size(162, 45)
+        Button9.TabIndex = 14
+        Button9.Text = "Font 2"
+        Button9.UseVisualStyleBackColor = True
+        ' 
+        ' Button10
+        ' 
+        Button10.Location = New Point(391, 369)
+        Button10.Name = "Button10"
+        Button10.Size = New Size(162, 45)
+        Button10.TabIndex = 15
+        Button10.Text = "Font 1"
+        Button10.UseVisualStyleBackColor = True
+        ' 
+        ' Button14
+        ' 
+        Button14.Location = New Point(29, 457)
+        Button14.Name = "Button14"
+        Button14.Size = New Size(162, 45)
+        Button14.TabIndex = 19
+        Button14.Text = "Visible"
+        Button14.UseVisualStyleBackColor = True
+        ' 
+        ' Button6
+        ' 
+        Button6.Location = New Point(381, 213)
+        Button6.Name = "Button6"
+        Button6.Size = New Size(162, 45)
+        Button6.TabIndex = 11
+        Button6.Text = "Left"
+        Button6.UseVisualStyleBackColor = True
+        ' 
+        ' Button7
+        ' 
+        Button7.Location = New Point(381, 275)
+        Button7.Name = "Button7"
+        Button7.Size = New Size(162, 45)
+        Button7.TabIndex = 12
+        Button7.Text = "Up"
+        Button7.UseVisualStyleBackColor = True
+        ' 
+        ' Button12
+        ' 
+        Button12.Location = New Point(559, 275)
+        Button12.Name = "Button12"
+        Button12.Size = New Size(162, 45)
+        Button12.TabIndex = 17
+        Button12.Text = "Down"
+        Button12.UseVisualStyleBackColor = True
+        ' 
+        ' Button13
+        ' 
+        Button13.Location = New Point(559, 213)
+        Button13.Name = "Button13"
+        Button13.Size = New Size(162, 45)
+        Button13.TabIndex = 18
+        Button13.Text = "Right"
+        Button13.UseVisualStyleBackColor = True
+        ' 
+        ' Button8
+        ' 
+        Button8.Location = New Point(391, 425)
+        Button8.Name = "Button8"
+        Button8.Size = New Size(162, 45)
+        Button8.TabIndex = 13
+        Button8.Text = "Font 3"
+        Button8.UseVisualStyleBackColor = True
+        ' 
+        ' Button11
+        ' 
+        Button11.Location = New Point(559, 425)
+        Button11.Name = "Button11"
+        Button11.Size = New Size(162, 45)
+        Button11.TabIndex = 16
+        Button11.Text = "Font 4"
+        Button11.UseVisualStyleBackColor = True
         ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(14F, 31F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(928, 421)
+        ClientSize = New Size(862, 625)
+        Controls.Add(Button14)
+        Controls.Add(Button13)
+        Controls.Add(Button12)
+        Controls.Add(Button11)
+        Controls.Add(Button10)
+        Controls.Add(Button9)
+        Controls.Add(Button8)
+        Controls.Add(Button7)
+        Controls.Add(Button6)
+        Controls.Add(Button5)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
         Controls.Add(Button1)
         Controls.Add(Label1)
         Controls.Add(clear1)
@@ -105,5 +248,18 @@ Partial Class Form1
     Friend WithEvents clear1 As Button
     Friend WithEvents Label1 As Label
     Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button14 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents Button13 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button11 As Button
 
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,17 +1,103 @@
 ﻿Public Class Form1
-    Private Sub exitBtn_Click(sender As Object, e As EventArgs) Handles exitBtn.Click
-        End
+
+    ' Sets the content of the text box
+    Private Sub helloBtn_Click(sender As Object, e As EventArgs) Handles helloBtn.Click
+        txt.Text = "Hello, Universe 2! " & Today
     End Sub
 
+    ' Clears text box content using assignment
     Private Sub clear1_Click(sender As Object, e As EventArgs) Handles clear1.Click
+        txt.Text = ""
+    End Sub
+
+    ' Clears text box using packaged method `Clear()`
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         txt.Clear()
     End Sub
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        txt.Text = "Goodbye, Cruel world"
+    ' **Manually** negating the `Multiline` value
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If txt.Multiline = True Then
+            txt.Multiline = False
+        Else
+            txt.Multiline = True
+        End If
+
+        ' We can also just negate it: (In VBA)
+        ' txt.Multiline = Not(txt.Multiline)
+        ' txt.Multiline = !txt.Multiline; (In C)
     End Sub
 
-    Private Sub helloBtn_Click(sender As Object, e As EventArgs) Handles helloBtn.Click
-        txt.Text = "Hello there!"
+    ' Negating the `Enabled` member using binary `Not` operator (equiv to !, bang)
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        txt.Enabled = Not (txt.Enabled)
+    End Sub
+
+    ' Assign and subtract 10 from `Left`
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        txt.Left -= 10
+    End Sub
+
+
+    ' Negating the `Visable` member using binary `Not` operator (equiv to !, bang)
+    Private Sub Button14_Click(sender As Object, e As EventArgs) Handles Button14.Click
+        txt.Visible = Not (txt.Visible)
+    End Sub
+
+    ' Adding 10 to `Left` (negation of the `txt.Left -= 10` operation)
+    Private Sub Button13_Click(sender As Object, e As EventArgs) Handles Button13.Click
+        txt.Left += 10
+    End Sub
+
+    ' Subtracting from `Top` to go down
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        txt.Top -= 10
+    End Sub
+
+    ' Adds to `Top` to move upwards
+    Private Sub Button12_Click(sender As Object, e As EventArgs) Handles Button12.Click
+        txt.Top += 10
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        If txt.BorderStyle = BorderStyle.Fixed3D Then
+            txt.BorderStyle = BorderStyle.FixedSingle
+        ElseIf txt.BorderStyle = BorderStyle.FixedSingle Then
+            txt.BorderStyle = BorderStyle.None
+        Else
+            txt.BorderStyle = BorderStyle.Fixed3D
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        If txt.TextAlign = HorizontalAlignment.Center Then
+            txt.TextAlign = HorizontalAlignment.Right
+        ElseIf txt.TextAlign = HorizontalAlignment.Right Then
+            txt.TextAlign = HorizontalAlignment.Left
+        Else
+            txt.TextAlign = HorizontalAlignment.Center
+        End If
+    End Sub
+
+    ' Change fonts:
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        txt.Font = New Font("MS Sans Serif", 12, FontStyle.Underline)
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        txt.Font = New Font("Times New Roman", 8, FontStyle.Regular)
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        txt.Font = New Font("MS Sans Serif", 10, FontStyle.Italic)
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+        txt.Font = New Font("Verdana", 16, FontStyle.Bold)
+    End Sub
+
+    Private Sub exitBtn_Click(sender As Object, e As EventArgs) Handles exitBtn.Click
+        End
     End Sub
 End Class
